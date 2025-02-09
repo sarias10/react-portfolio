4395c4d5e356557e0645019e45540daf196d476f
Sergio Arias <s-andres140@hotmail.com>
2025-02-09 11:41:52 -0500

    chore: :memo: update Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hola, soy Sergio</h1>
         <p className={styles.description}>
-            Soy desarrollador Fullstack con conocimientos en React, NodeJS, Express y bases de datos Mongo.   
+        Soy desarrollador Fullstack con experiencia en React, Node.js, Express y bases de datos MongoDB.
         </p>
         <a href="#contact" className={styles.contactBtn}>
           Contactame
